7f8cf00cd2ca0cf521f36b5f6f3bc8eac404fc23
ChaeYeon Kim <soqk127@naver.com>
2023-03-30 16:31:37 +0900

    SignUp Page

---
+++ src/components/Main/Footer.js
@@ -0,0 +1,11 @@
+import "./Main.css";
+
+function Footer() {
+    return(
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">footer info</p></div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/pages/Signup.js
@@ -0,0 +1,13 @@
+import SignupForm from "../components/SignupForm/SignupForm";
+import "./pages.css";
+
+function Signup() {
+    return(
+        <>
+        <p className="sign-p">회원가입</p>
+        <SignupForm/>
+        </>
+    );
+}
+
+export default Signup;
