0f979e1e262d32fd566e47f430939e840a2f4e5d
minji <ipink89@naver.com>
2023-03-31 00:48:20 +0900

    commit_mj

---
+++ src/components/Chart/MyInfo.js
@@ -0,0 +1,37 @@
+import "./Chart.css";
+
+function Info() {
+    return(
+        <>
+            <table
+            border="0"
+            width="990"
+            height="265"
+            cellspacing="10">
+                <tr>
+                    <td className="table11">이름</td>
+                    <td className="table22">홍맹구</td>
+                </tr>
+                <tr>
+                    <td className="table11">성별</td>
+                    <td className="table22">남자</td>
+                </tr>
+                <tr>
+                    <td className="table11">생년월일</td>
+                    <td className="table22">2023년 3월 21일</td>
+                </tr>
+                <tr>
+                    <td className="table11">아이디</td>
+                    <td className="table22">Hong</td>
+                </tr>
+                <tr>
+                    <td className="table11">거주지역</td>
+                    <td className="table22">서울특별시</td>
+                </tr>
+        </table>
+
+        </>
+    );
+}
+
+export default Info;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,3 +1,4 @@
+import Banner from "../components/Main/Banner";
 import Best from "../components/Main/Best";
 import Clinics from "../components/Main/Clinics";
 import Footer from "../components/Main/Footer";
@@ -6,7 +7,7 @@ import Display from "../components/Main/Display";
 function Main() {
     return (
         <>
-        <Display/>
+        <Banner/>
         <Clinics/>
         <Best/>
         <Footer/>
