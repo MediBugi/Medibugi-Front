3bea8d6a61ab7ef728e5453b6448361c1d7db100
ooinl77 <ooinl77@naver.com>
2023-04-04 16:39:18 +0900

    1

--- src/pages/SearchDocter.js
+++ src/pages/SearchDocter.js
@@ -2,14 +2,14 @@ import Searchdoc from "../components/Content/Searchdoc";
 import "./pages.css";
 
 function SearchDoctor() {
-    return(
-        <>
-        <div className="main">
+  return (
+    <>
+      <div className="main">
         <p className="p">의사·병원찾기</p>
-        <Searchdoc/>
-        </div>
-        </>
-    );
+        <Searchdoc />
+      </div>
+    </>
+  );
 }
 
 export default SearchDoctor;
